25a9ff1cfe818916a4dd68dd62ce247f647e0a27
XtophB <xtophb@gmail.com>
2026-07-16 01:13:09 +0200

    feat: add employee management system
    manager singleton now has the ability to hire, fire and pay employees

---
+++ amore-mio/src/main/java/com/amoremio/employee/EmployeeManagerSingleton.java
@@ -0,0 +1,36 @@
+package com.amoremio.employee;
+
+import com.amoremio.store.Branch;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class EmployeeManagerSingleton {
+  private static EmployeeManagerSingleton INSTANCE;
+  private final List<Employee> globalEmployees = new ArrayList<>();
+
+  private EmployeeManagerSingleton() {}
+
+  public static EmployeeManagerSingleton getInstance() {
+    if (INSTANCE == null) {
+      INSTANCE = new EmployeeManagerSingleton();
+    }
+    return INSTANCE;
+  }
+
+  public void hireEmployee(Branch branch, Position position, EmployeeFactory employeeFactory) {
+    Employee employee = employeeFactory.create(position);
+    branch.addEmployee(employee);
+    globalEmployees.add(employee);
+  }
+
+  public void removeEmployee(Branch branch, Employee employee) {
+    branch.removeEmployee(employee);
+    globalEmployees.remove(employee);
+  }
+
+  public void payEmployees() {
+    for (Employee employee : globalEmployees) {
+      employee.getPaid();
+    }
+  }
+}
--- amore-mio/src/main/java/com/amoremio/store/Branch.java
+++ amore-mio/src/main/java/com/amoremio/store/Branch.java
@@ -1,3 +1,28 @@
 package com.amoremio.store;
 
-public class Branch {}
+import com.amoremio.employee.Employee;
+import com.amoremio.ingredients.IngredientFactory;
+import java.util.List;
+
+public class Branch {
+  List<Employee> employees;
+  Storage storage;
+  Supplier supplier;
+  IngredientFactory ingredientFactory;
+  City city;
+
+  Branch(Storage storage, Supplier supplier, IngredientFactory ingredientFactory, City city) {
+    this.storage = storage;
+    this.supplier = supplier;
+    this.ingredientFactory = ingredientFactory;
+    this.city = city;
+  }
+
+  public void addEmployee(Employee employee) {
+    employees.add(employee);
+  }
+
+  public void removeEmployee(Employee employee) {
+    employees.remove(employee);
+  }
+}
