d315f9977ad843d88234ff96916adb8d4881b547
XtophB <xtophb@gmail.com>
2026-07-16 00:34:17 +0200

    feat: add storage implementation
    Storage now has methods for restocking and retrieving ingredients

--- amore-mio/src/main/java/com/amoremio/store/Storage.java
+++ amore-mio/src/main/java/com/amoremio/store/Storage.java
@@ -1,3 +1,37 @@
 package com.amoremio.store;
 
-public class Storage {}
+import com.amoremio.ingredients.Ingredient;
+import com.amoremio.ingredients.IngredientFactory;
+import com.amoremio.ingredients.IngredientName;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+
+public class Storage {
+  private final EnumMap<IngredientName, ArrayList<Ingredient>> inventory;
+  private final Supplier supplier;
+
+  public Storage(Supplier supplier) {
+    this.inventory = new EnumMap<>(IngredientName.class);
+    this.supplier = supplier;
+
+    for (IngredientName name : IngredientName.values()) {
+      inventory.put(name, new ArrayList<Ingredient>());
+    }
+  }
+
+  public void restock(List<Ingredient> ingredientList) {
+    for (Ingredient ingredient : ingredientList) {
+      inventory.get(ingredient.getName()).add(ingredient);
+    }
+  }
+
+  public Ingredient getIngredient(IngredientName name) {
+    if (inventory.get(name).size() < 5) {
+      supplier.deliverIngredients(this, name, 10, 7);
+    }
+    Ingredient retrievedItem = inventory.get(name).getFirst();
+    inventory.get(name).removeFirst();
+    return retrievedItem;
+  }
+}
