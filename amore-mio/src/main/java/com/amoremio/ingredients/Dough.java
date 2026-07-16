61d634bf7b610ac400a8a5f57eac15fa164b4153
XtophB <xtophb@gmail.com>
2026-07-16 13:03:29 +0200

    refactor: remove dough type, doughs are generic

--- amore-mio/src/main/java/com/amoremio/ingredients/Dough.java
+++ amore-mio/src/main/java/com/amoremio/ingredients/Dough.java
@@ -3,10 +3,8 @@ package com.amoremio.ingredients;
 public class Dough extends Ingredient {
   private static final float PRICE = 0.5f;
   private static final IngredientName name = IngredientName.DOUGH;
-  private final DoughType doughType;
 
-  public Dough(int expiryDayOffset, DoughType doughType) {
+  public Dough(int expiryDayOffset) {
     super(name, PRICE, expiryDayOffset);
-    this.doughType = doughType;
   }
 }
