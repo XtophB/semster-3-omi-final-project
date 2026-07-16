69d5dc68fc8041d0ee546593e43751e2a6be5fd4
XtophB <xtophb@gmail.com>
2026-07-16 12:04:21 +0200

    feat: add dough type, WIP

--- amore-mio/src/main/java/com/amoremio/ingredients/Dough.java
+++ amore-mio/src/main/java/com/amoremio/ingredients/Dough.java
@@ -3,8 +3,10 @@ package com.amoremio.ingredients;
 public class Dough extends Ingredient {
   private static final float PRICE = 0.5f;
   private static final IngredientName name = IngredientName.DOUGH;
+  private final DoughType doughType;
 
-  public Dough(int expiryDayOffset) {
+  public Dough(int expiryDayOffset, DoughType doughType) {
     super(name, PRICE, expiryDayOffset);
+    this.doughType = doughType;
   }
 }
---
+++ amore-mio/src/main/java/com/amoremio/ingredients/DoughType.java
@@ -0,0 +1,7 @@
+package com.amoremio.ingredients;
+
+public enum DoughType {
+  THICK,
+  THIN,
+  FLAT_BREAD
+}
