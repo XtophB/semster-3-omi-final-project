0a39d51f4ccd522bb959d1c43d4da0c59988e05e
XtophB <xtophb@gmail.com>
2026-07-17 16:02:40 +0200

    feat: add coordinator implementation

--- amore-mio/src/main/java/com/amoremio/employee/roles/Coordinator.java
+++ amore-mio/src/main/java/com/amoremio/employee/roles/Coordinator.java
@@ -1,9 +1,50 @@
 package com.amoremio.employee.roles;
 
 import com.amoremio.employee.Employee;
+import com.amoremio.ingredients.Ingredient;
+import com.amoremio.ingredients.IngredientName;
+import com.amoremio.order.Order;
+import com.amoremio.order.OrderProcess;
+import com.amoremio.order.OrderedPizza;
+import com.amoremio.store.Storage;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+
 
 public class Coordinator extends Employee {
+  private final EnumMap<IngredientName, Integer> orderRequirements;
   public Coordinator(int basePay, float payMultiplier) {
     super(basePay, payMultiplier);
+    this.orderRequirements = new EnumMap<>(IngredientName.class);
+  }
+
+  public void checkOrder(Storage storage, OrderProcess orderProcess) {
+    Order order = orderProcess.getOrder();
+    calculateRequiredToppings(order);
+    checkStorage(storage);
+  }
+  private void calculateRequiredToppings(Order order) {
+    orderRequirements.clear();
+    List<OrderedPizza> orderedPizzas = order.getPizzaOrder();
+    for (OrderedPizza orderedPizza : orderedPizzas) {
+      List<Ingredient> toppings = orderedPizza.getToppings();
+      for (Ingredient topping : toppings) {
+        orderRequirements.put(topping.getName(), orderRequirements.get(topping.getName()) + 1);
+      }
+    }
+  }
+
+  private void checkStorage(Storage storage) {
+    EnumMap<IngredientName, ArrayList<Ingredient>> inventory = storage.getInventory();
+    for (IngredientName name : orderRequirements.keySet()) {
+      System.out.println("Ingredient: " + name + ", Required: " + orderRequirements.get(name));
+      System.out.println("Ingredient: " + name + ", In Storage: " + inventory.get(name).size());
+      if (inventory.get(name).size() < orderRequirements.get(name)) {
+        System.out.println("Not enough ingredients");
+        System.out.println("Calling supplier for " + name);
+        storage.callSupplier(name);
+      }
+    }
   }
 }
