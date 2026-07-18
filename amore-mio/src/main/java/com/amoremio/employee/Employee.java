de354398c365bbb03f15d47d616fe6daab8b86fd
XtophB <xtophb@gmail.com>
2026-07-18 15:34:20 +0200

    feat: allow employee to be busy

--- amore-mio/src/main/java/com/amoremio/employee/Employee.java
+++ amore-mio/src/main/java/com/amoremio/employee/Employee.java
@@ -9,12 +9,14 @@ public abstract class Employee {
   private int basePay;
   private float payMultiplier;
   private float balance;
+  private boolean busy = false;
 
   public Employee(int basePay, float payMultiplier) {
     this.basePay = basePay;
     this.payMultiplier = payMultiplier;
     this.balance = 0;
   }
+
   public void getPaid() {
     balance += basePay * payMultiplier;
   }
